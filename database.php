1b54829662d8377d53aaf274941e7b1ca8b35218
sabadoryo <ernarernar25>
2021-09-05 07:03:17 +0600

    fix

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private $host = "127.0.0.1";
+    private $host = "localhost";
     private $database_name = "megabit";
     private $username = "yernar";
     private $password = "password";
